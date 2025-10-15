eebec97627968633fa0cbaa2b08b9d523400b871
mmaslov <mmaslov@131.ru>
2025-10-16 00:56:55 +0300

    v2

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
-module github.com/maxXYZ/test_lib
+module github.com/maxXYZ/test_lib/v2
 
-go 1.22.2
+go 1.24.0
+
+toolchain go1.24.9
+
+require golang.org/x/exp v0.0.0-20251009144603-d2f985daa21b // indirect
--- test-lib.go
+++ test-lib.go
@@ -1,7 +1,15 @@
 package test_lib
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add two integers. See more on [google]
 // [google]: https://google.com
-func Add(x int, y int) int {
+func Add[I int](x I, y I) I {
 	return x + y
 }
